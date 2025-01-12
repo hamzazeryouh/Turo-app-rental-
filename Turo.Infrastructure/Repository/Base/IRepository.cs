a62e76dc672c50cb3d5b959db37a126a042438b3
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-12 13:40:53 +0100

    repo pattern

---
+++ Turo.Infrastructure/Repository/Base/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Turo.Application.Helpers;
+
+namespace Turo.Infrastructure.Repository.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<PaginatedList<T>> GetAllAsync(int pageNumber, int pageSize);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+
+}
---
+++ Turo.Infrastructure/Repository/Base/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Turo.Application.Helpers;
+
+namespace Turo.Infrastructure.Repository.Base
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task<T> GetByIdAsync(int id)=> await _dbSet.FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync()=>  await _dbSet.ToListAsync();
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) => await _dbSet.Where(predicate).ToListAsync();
+        public async Task AddAsync(T entity)=> await _dbSet.AddAsync(entity);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Delete(T entity)=> _dbSet.Remove(entity);
+
+        public async Task<PaginatedList<T>> GetAllAsync(int pageNumber, int pageSize)
+        {
+            var totalCount = await _dbSet.CountAsync();
+            var items = await _dbSet
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedList<T>(items, totalCount, pageNumber, pageSize);
+        }
+
+    }
+
+}
