e02164fce31e971f91ba09778b2df440baa827ff
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-16 15:12:55 +0100

    remove neccecary code

--- Turo/Program.cs
+++ Turo/Program.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using Turo.Application.Maping;
 using Turo.Domain.Entities;
 using Turo.Infrastructure.Persistence;
+using Turo.Infrastructure.Repository;
 using Turo.Infrastructure.Repository.Base;
 using Turo.Infrastructure.Repository.Cars;
 using Turo.Infrastructure.UnitOfWork;
@@ -16,25 +17,14 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddScoped<IGenericRepository<Car>, GenericRepository<Car>>();
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAgencyRepository, AgencyRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
-
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
 
 app.Run();
 
