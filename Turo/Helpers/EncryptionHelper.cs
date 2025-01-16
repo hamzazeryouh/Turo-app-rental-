f755629e91504861b4c8ec09fbc03c19ca082496
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-16 16:29:06 +0100

    add this

---
+++ Turo/Helpers/EncryptionHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Turo.Helpers
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class EncryptionHelper
+    {
+        private static readonly string key = "your-encryption-key"; // Make sure to store this securely
+        private static readonly string iv = "your-initialization-vector";
+
+        public static string Encrypt(string plainText)
+        {
+            using (var aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                using (var sw = new StreamWriter(cs))
+                {
+                    sw.Write(plainText);
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+    }
+
+}
