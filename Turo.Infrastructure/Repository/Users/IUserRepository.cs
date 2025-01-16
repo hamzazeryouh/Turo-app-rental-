63987defd38ec3631aec3bc9f5a073d9a5d998ff
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-16 15:02:16 +0100

    add users repo

---
+++ Turo.Infrastructure/Repository/Users/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Turo.Infrastructure.Repository
+{
+    using Turo.Domain.Entities;
+    using Turo.Infrastructure.Repository.Base;
+    public interface IUserRepository : IGenericRepository<User>
+    {
+    }
+}
---
+++ Turo.Infrastructure/Repository/Users/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Turo.Infrastructure.Repository
+{
+
+    using Turo.Infrastructure.Persistence;
+    using Turo.Infrastructure.Repository.Base;
+    using Turo.Domain.Entities;
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
