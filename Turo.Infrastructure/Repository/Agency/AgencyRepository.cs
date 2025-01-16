3f14cb042e92bd6ed5a4dfa1dadbd687743d4f07
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-16 15:01:52 +0100

    agence repo

---
+++ Turo.Infrastructure/Repository/Agency/AgencyRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turo.Domain.Entities;
+using Turo.Infrastructure.Persistence;
+using Turo.Infrastructure.Repository.Base;
+
+namespace Turo.Infrastructure.Repository
+{
+    public class AgencyRepository : GenericRepository<Agency>, IAgencyRepository
+    {
+        public AgencyRepository(AppDbContext context) : base(context) { }
+
+        public async Task<Agency> GetAgencyWithLocationsAsync(int agencyId)
+        {
+            return await _context.Agencies
+                .Include(a => a.Locations)
+                .FirstOrDefaultAsync(a => a.Id == agencyId);
+        }
+    }
+}
---
+++ Turo.Infrastructure/Repository/Agency/IAgencyRepository.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Turo.Infrastructure.Repository
+{
+    using Turo.Domain.Entities;
+    using Turo.Infrastructure.Repository.Base;
+    public interface IAgencyRepository : IGenericRepository<Agency>
+    {
+        Task<Agency> GetAgencyWithLocationsAsync(int agencyId);
+    }
+}
