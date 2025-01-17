885dfcd50950e0020250749bfd76c50eae5cd5eb
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-17 16:47:01 +0100

    car controller with error

---
+++ Turo/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Turo.Application.Commands.Cars;
+using Turo.Application.Queries.Car;
+using Turo.Application.Queries.Cars;
+using Turo.Application.Services;
+using Turo.Domain.Entities;
+
+namespace Turo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarController : BaseController<Car, GetAllCarsQuery, GetByIdCarQuery, CreateCarCommand, UpdateCarCommand, DeleteCarCommand>
+    {
+        public CarController(ITranslationService translationService, IMediator mediator) : base(translationService, mediator)
+        {
+        }
+    }
+}
