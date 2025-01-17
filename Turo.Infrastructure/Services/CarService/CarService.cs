1c5dfde79053d0092b7c953c9c313e861247ebcc
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-17 15:37:33 +0100

    Create service cars

---
+++ Turo.Infrastructure/Services/CarService/CarService.cs
@@ -0,0 +1,13 @@
+﻿namespace Turo.Infrastructure.Services.CarService
+{
+    using AutoMapper;
+    using Turo.Application.Dtos.Cars;
+    using Turo.Infrastructure.UnitOfWork;
+    using Turo.Domain.Entities;
+    public class CarService : BaseService<Car, CreateCarDTo, UpdateCarDTo, CarDto>, ICarService
+    {
+        public CarService(Repository.Base.IGenericRepository<Car> repository, IUnitOfWork unitOfWork, IMapper mapper) : base(repository, unitOfWork, mapper)
+        {
+        }
+    }
+}
---
+++ Turo.Infrastructure/Services/CarService/ICarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turo.Application.Dtos.Cars;
+using Turo.Application.Interfaces;
+using Turo.Domain.Entities;
+
+namespace Turo.Infrastructure.Services.CarService
+{
+    public interface  ICarService : IBaseService<CreateCarDTo,UpdateCarDTo,CarDto>
+    {
+    }
+}
