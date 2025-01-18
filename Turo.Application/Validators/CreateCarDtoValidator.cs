07154c42eb9d05d9d55ca43d8c1039fc9f1a83ef
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-18 19:19:20 +0100

    First validate example

---
+++ Turo.Application/Validators/CreateCarDtoValidator.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turo.Application.Dtos.Cars;
+using Turo.Application.Services;
+using Turo.Domain.Entities;
+
+namespace Turo.Application.Validators
+{
+
+
+    public class CreateCarDtoValidator : AbstractValidator<Car>
+    {
+        private readonly ITranslationService _translationService;
+
+        public CreateCarDtoValidator(ITranslationService translationService)
+        {
+            _translationService = translationService;
+
+            RuleFor(car => car.Make)
+                .NotEmpty().WithMessage(_ => _translationService.GetTranslation("MakeRequired"))
+                .MaximumLength(50).WithMessage(_ => _translationService.GetTranslation("MakeMaxLength"));
+
+            RuleFor(car => car.Model)
+                .NotEmpty().WithMessage(_ => _translationService.GetTranslation("ModelRequired"))
+                .MaximumLength(50).WithMessage(_ => _translationService.GetTranslation("ModelMaxLength"));
+
+            RuleFor(car => car.VIN)
+                .NotEmpty().WithMessage(_ => _translationService.GetTranslation("VINRequired"))
+                .Matches(@"^[A-HJ-NPR-Z0-9]{17}$").WithMessage(_ => _translationService.GetTranslation("VINFormat"));
+
+            RuleFor(car => car.Year)
+                .NotNull().WithMessage(_ => _translationService.GetTranslation("YearRequired"))
+                .InclusiveBetween(1886, DateTime.UtcNow.Year)
+                .WithMessage(_ => _translationService.GetTranslation("YearRange"));
+
+            RuleFor(car => car.LicensePlate)
+                .NotEmpty().WithMessage(_ => _translationService.GetTranslation("LicensePlateRequired"))
+                .MaximumLength(10).WithMessage(_ => _translationService.GetTranslation("LicensePlateMaxLength"));
+
+            RuleFor(car => car.PricePerDay)
+                .GreaterThan(0).WithMessage(_ => _translationService.GetTranslation("PricePerDayMin"));
+
+            RuleFor(car => car.Mileage)
+                .GreaterThanOrEqualTo(0).WithMessage(_ => _translationService.GetTranslation("MileageMin"));
+
+            RuleFor(car => car.Seats)
+                .InclusiveBetween(1, 10).WithMessage(_ => _translationService.GetTranslation("SeatsRange"));
+
+            RuleFor(car => car.Latitude)
+                .InclusiveBetween(-90, 90).WithMessage(_ => _translationService.GetTranslation("LatitudeRange"));
+
+            RuleFor(car => car.Longitude)
+                .InclusiveBetween(-180, 180).WithMessage(_ => _translationService.GetTranslation("LongitudeRange"));
+        }
+    }
+
+}
