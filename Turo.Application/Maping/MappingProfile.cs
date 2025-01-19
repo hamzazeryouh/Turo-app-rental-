7cb4c093c2fa5acf65dae4a4b9ccd04fa54a0222
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-20 00:04:14 +0100

    add maping

--- Turo.Application/Maping/MappingProfile.cs
+++ Turo.Application/Maping/MappingProfile.cs
@@ -13,6 +13,12 @@ namespace Turo.Application.Maping
                 .ForMember(dest => dest.PhotoUrls, opt => opt.MapFrom(src => src.Photos.Select(p => p.Url)))
                 .ForMember(dest => dest.AverageRating, opt => opt.MapFrom(src => src.Reviews.Any() ? src.Reviews.Average(r => r.Rating) : 0))
                 .ForMember(dest => dest.ReviewCount, opt => opt.MapFrom(src => (src.Reviews.Count)));
+            CreateMap<CreateCarDTo, Car>()
+               .ForMember(dest => dest.Photos, opt => opt.Ignore()); // Assuming photos are handled separately
+
+            // Mapping from UpdateCarDto to Car
+            CreateMap<UpdateCarDTo, Car>()
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null)); // Only update non-null properties
         }
     }
 }
