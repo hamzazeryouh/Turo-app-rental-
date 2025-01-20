3d79c3cf7e2d7c84d2b11ba1f8f5d92011b51b66
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-20 17:11:41 +0100

    change this class from application layer to infastrcter layer
    Add PaginatedList<T> class to Turo.Infrastructure.Helpers
    
    Introduced a new namespace `Turo.Infrastructure.Helpers`.
    Added a generic class `PaginatedList<T>` with properties:
    - `IEnumerable<T> Items`
    - `int TotalCount`
    - `int TotalPages`
    - `int PageNumber`
    - `int PageSize`
    Added a constructor to initialize these properties and calculate `TotalPages`.
    Included boolean properties `HasPreviousPage` and `HasNextPage` to determine page navigation availability.

---
+++ Turo.Infrastructure/Helpers/PaginatedList.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Turo.Infrastructure.Helpers
+{
+    public class PaginatedList<T>
+    {
+        public IEnumerable<T> Items { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageNumber { get; private set; }
+        public int PageSize { get; private set; }
+
+        public PaginatedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = count;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+
+}
