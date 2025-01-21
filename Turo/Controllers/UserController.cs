02deae934a4335561f4e839ac5d2205a863ed7e7
hamza zeyouh <zeryouhbusiness@gmail.com>
2025-01-21 18:08:29 +0100

    add user controller
    Add UserController with GET endpoint for "api/User"
    
    Introduce a new UserController class in the Turo.Controllers namespace.
    The controller is decorated with [Route("api/User")] and [ApiController]
    attributes. A GET method is added to handle HTTP GET requests, returning
    a 200 OK status. Necessary namespaces for ASP.NET Core MVC are imported.

---
+++ Turo/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turo.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+    }
+}
